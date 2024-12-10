7a42caf57e401602bbbcb6745b2b233354b701a3
ykpythemind <yukibukiyou@gmail.com>
2024-12-10 12:43:10 +0900

    s

--- table_count.rb
+++ table_count.rb
@@ -1,22 +1,4 @@
 require 'bundler/setup'
-require 'prism'
-require 'pathname'
-
-class Visitor < Prism::Visitor
-  attr_reader :count
-
-  def initialize
-    super()
-    @count = 0
-  end
-
-  def visit_call_node(node)
-    super
-    if node.name == :create_table
-      @count += 1
-    end
-  end
-end
 
 rails_root = ARGV[0]
 if rails_root.nil?
@@ -24,8 +6,6 @@ if rails_root.nil?
   abort
 end
 
-parse_result = Prism.parse_file(Pathname.new(rails_root).join('db/schema.rb').to_s)
-visitor = Visitor.new
-parse_result.value.accept(visitor)
-
-puts visitor.count
+Dir.chdir(rails_root) do
+  puts `grep create_table db/schema.rb | wc -l`.strip
+end
